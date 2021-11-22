7e3510a2476a01296c58aea56c0e960897f698b8
raul g <rglepe@gmail.com>
2021-11-22 11:31:40 +0100

    added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { BookModule } from './book/book.module'
 import { UserModule } from './user/user.module'
 import { LoanModule } from './loan/loan.module'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import configuration from './config/config'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
     BookModule,
     UserModule,
     LoanModule,
