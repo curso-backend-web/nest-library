ce71d19ba378b21bf9c1fbdcdb853f598b734198
raul g <rglepe@gmail.com>
2021-12-09 10:10:22 +0100

    new version

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,23 @@
 import { registerAs } from '@nestjs/config'
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
 
 export default registerAs('db', () => {
-  return {
+  const connectionOption: MysqlConnectionOptions = {
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
     entities: ['dist/**/*.entity.js'],
     synchronize: false,
   }
+
+  if (process.env.CLEARDB_DATABASE_URL) {
+    Object.assign(connectionOption, { url: process.env.CLEARDB_DATABASE_URL })
+  } else {
+    Object.assign(connectionOption, {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+    })
+  }
+  return connectionOption
 })
